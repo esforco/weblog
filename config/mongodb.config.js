3677c765675bb9c99e49b0c485a5415caec821e0
esforco <esforco1989@gmail.com>
2020-03-26 07:04:02 +0900

    no message

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   CONNECTION_URL: MONGODB_URI,
-  mongodb+srv://user:<password>@cluster0-f7p76.mongodb.net/test?retryWrites=true&w=majority
   DATABASE: "weblog",
   OPTIONS: {
     family: 4
