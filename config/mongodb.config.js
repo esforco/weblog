69aa584343c2b4d185595916c2306044107b682c
esforco <esforco1989@gmail.com>
2020-03-26 07:32:38 +0900

    update Mongo settings

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CONNECTION_URL: "mongodb+srv://user1:user1@cluster0-f7p76.mongodb.net/test?retryWrites=true&w=majority",
+  CONNECTION_URL: MONGODB_URI,
   DATABASE: "weblog",
   OPTIONS: {
     family: 4
