b128d541988231ed09ce1a06717bfade1e49ef0c
esforco <esforco1989@gmail.com>
2020-03-26 07:17:49 +0900

    no message

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CONNECTION_URL: "mongodb+srv://user:user@cluster0-f7p76.mongodb.net/test?retryWrites=true&w=majority",
+  CONNECTION_URL: "mongodb+srv://user1:user1@cluster0-f7p76.mongodb.net/test?retryWrites=true&w=majority",
   DATABASE: "weblog",
   OPTIONS: {
     family: 4
